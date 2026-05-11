ce331f01e7542cfb1b43c7df494e8aaab9cfe454
Orlando Monaco <monacos@monacos.us>
2026-05-11 10:05:33 -0400

    Added Azure Web API URI

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,9 @@
 export const environment = {
 
     production: false,
-    apiUrl: 'http://localhost/MonacosUsWebAPI/api/',
+    //apiUrl: 'http://localhost/MonacosUsWebAPI/api/',
     //apiUrl:'http://www.monacos.us/MonacosUSWebAPI/api/',
+    apiUri: 'https://monacosuswebapi-bmgpbyaja5dydeaa.canadacentral-01.azurewebsites.net/api/',
     guestUserName: "guestuser",
     guestUserPassword: "GoWestYoungMan_1972"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,8 @@ export const environment = {
 
     production: true,
     //apiUrl: 'http://localhost/MonacosUsWebAPI/api/',
-    apiUrl:'http://www.monacos.us/MonacosUSWebAPI/api/',
+    //apiUrl:'http://www.monacos.us/MonacosUSWebAPI/api/',
+    apiUri: 'https://monacosuswebapi-bmgpbyaja5dydeaa.canadacentral-01.azurewebsites.net/api/',
     guestUserName: "guestuser",
     guestUserPassword: "GoWestYoungMan_1972"
 };
